a7588fd6becbadcaf5ea9f00cf387c568bdc0cae
Igor Moura <igormoura>
2013-07-18 17:41:11 -0300

    Firt commit

---
+++ themes/bootstrap/views/layouts/main.php
@@ -0,0 +1,70 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+	<?php Yii::app()->bootstrap->register(); ?>
+</head>
+
+<body>
+
+<?php $this->widget('bootstrap.widgets.TbNavbar',array(
+    'type'=>'inverse',
+    'collapse'=>true,
+      'fixed' => 'top',
+    'items'=>array(
+        array(
+            'class'=>'bootstrap.widgets.TbMenu',
+            'items'=>array(
+                array('label'=>'Home', 'url'=>array('/site/index')),
+                array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+                array('label'=>'Contact', 'url'=>array('/site/contact')),
+                array('label'=>'Users', 'url'=>array('/users/admin'))
+            ),
+        ),
+        array(
+            'class'=>'bootstrap.widgets.TbMenu',
+            'htmlOptions'=>array('class'=>'pull-right'),            
+            'items'=>array( 
+                '---',
+                array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+                array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+            ),
+        ),
+        '<form class="navbar-search pull-right" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
+
+    ),
+
+    
+)); ?>
+
+<div class="container" id="page">
+
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer" class="well">
+		Copyright &copy; <?php echo date('Y'); ?> by <a href="http://www.secrel.com.br/">SecrelNet.</a> 
+                <div class="pull-right">
+		All Rights Reserved.
+		<?php echo Yii::powered(); ?>
+                </div>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
