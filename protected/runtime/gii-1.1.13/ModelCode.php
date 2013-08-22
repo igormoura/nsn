7ee87bac71c2ed71c0240a498bf89bc2f8c48942
Igor Moura <igormoura>
2013-08-22 18:02:51 -0300

    Revert "first commit"
    This reverts commit 88a9aa3ed9ae35f36f2bc18a2c7cc4bda8e1a5c1.

--- protected/runtime/gii-1.1.13/ModelCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'connectionId' => 'secondb',
-  'tablePrefix' => '',
-  'modelPath' => 'application.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
-);
