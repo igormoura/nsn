e57551f862328c484b739fd5c30256aaf340cae5
Igor Moura <igormoura>
2013-09-26 17:32:48 -0300

    test commit netbeans

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -1,25 +1,25 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
         <script type="text/javascript" src="/nsn/assets/c5f5426e/js/date.min.js"></script>
         <script type="text/javascript" src="/nsn/assets/c5f5426e/js/jquery.min.js"></script>
         <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
-	<?php Yii::app()->bootstrap->register(); ?>
-        <?php $translate=Yii::app()->translate; ?>
-</head>
-    
-<body>  
-    <?php $this->renderPartial('//components/_TbNavbarMain')?> 
+        <?php Yii::app()->bootstrap->register(); ?>
+        <?php $translate = Yii::app()->translate; ?>
+    </head>
 
-    <div class="container" id="page">
-        <?php echo $content; ?>
-        <div class="clear"></div>
-    </div><!-- page -->
-</body>
+    <body>  
+        <?php $this->renderPartial('//components/_TbNavbarMain') ?> 
+
+        <div class="container" id="page">
+            <?php echo $content; ?>
+            <div class="clear"></div>
+        </div><!-- page -->
+    </body>
 </html>
